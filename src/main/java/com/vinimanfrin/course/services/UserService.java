720f8f33f39ea18bccd5dd5ba5d15dd3f4d99c9c
vinimanfrin <viniciusmanfrin2019@gmail.com>
2023-07-31 15:49:47 -0300

    Service layer, component registration

---
+++ src/main/java/com/vinimanfrin/course/services/UserService.java
@@ -0,0 +1,26 @@
+package com.vinimanfrin.course.services;
+
+import com.vinimanfrin.course.entites.User;
+import com.vinimanfrin.course.repositories.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Component;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class UserService {
+
+    @Autowired
+    UserRepository repository ;
+
+    public List<User> findAll(){
+        return repository.findAll();
+    }
+    public User findByID(Long id){
+        Optional<User> obj = repository.findById(id);
+        return obj.get();
+    }
+
+}
